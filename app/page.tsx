a217aa35b0ff7a7e3950ea33ebd226060e59c447
sagarp-c <sagarp.cvr@gmail.com>
2024-12-21 12:35:24 +0530

    first pahse ok

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Hello! I'm [Your Name], a passionate developer specializing in [Your Specialization].</p>
+      <p>Skills: JavaScript, TypeScript, React, Next.js, etc.</p>
+    </main>
+  );
+}
---
+++ models/project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+    id: string;
+    title: string;
+    description: string;
+    technologies: string[];
+    image: string;
+    link: string;
+  }
+  
